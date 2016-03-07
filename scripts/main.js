2057a37b97fda627e33a19f2560dc53221fac7a0
Will Cook <wellsee83@gmail.com>
2016-03-07 14:18:17 -0600

    Adventurer Mode

--- scripts/main.js
+++ scripts/main.js
@@ -2,5 +2,12 @@ var noun = prompt('Enter a noun.');
 var verb = prompt('Enter a verb.');
 var adjective = prompt('Enter an adjective.');
 var adverb = prompt('Enter an adverb.');
+var verb2 = prompt('Enter a verb.');
+var noun2 = prompt('Enter a noun.');
+var adjective2 = prompt('Enter an adjective.');
+var verb3 = prompt('Enter a verb.');
+var adverb3 = prompt('Enter an adverb.');
 
-console.log('Do you '+ verb +' your '+ adjective +' '+ noun +' '+ adverb + '?  How crazy?!');
+console.log('Do you '+ verb +' your '+ adjective +' '+ noun +' '+ adverb + '?  How crazy?!  '
+	+ 'Well maybe if you ' + verb2 + ' with your ' + adjective2 + ' ' + noun2 + 
+	 ' then you could ' + verb3 + ' more ' + adverb3 + '.');
